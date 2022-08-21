e1a6a411eee39de24f4b7fbd93d2b09e1b74da01
Dario Britos <dariobritos1605@gmail.com>
2022-08-21 18:55:29 -0300

    primer clase de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const imagen = "led-zeppelin.jpg";
+  const titulo = "Mi primer app con react";
+  const subtitulo = "Esta es mi banda preferida"
+  const descripcion = "Se llama LED ZEPPELIN ";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card mb3 '>
+  <h3 className="card-header">{titulo}</h3>
+  <div className="card-body">
+    <h5 className="card-title">{subtitulo}</h5>
+  </div>
+  <img src={imagen} className="d-block user-select-none" width="40%" height="50%" />
+  <div className="card-body">
+    <h5 className="card-title">{descripcion}</h5>
+  </div>
     </div>
   );
 }
